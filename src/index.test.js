0dec0cb732cdb1e7a8ba01ae0ab69343c3d5bf39
Ben Brandt <benjamin.j.brandt@gmail.com>
2016-05-09 14:17:14 -0700

    Add test coverage for library

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,24 @@ const expect = require('chai').expect;
 const starWars = require('./index');
 
 describe('starwars-names', () => {
-  it('should work!', () => {
-    expect(true).to.be.true;
+  describe('all', () => {
+    it('should be an array of strings', () => {
+      function isArrayOfStrings(array) {
+        return array.every(item => typeof item === 'string');
+      }
+
+      expect(starWars.all).to.satisfy(isArrayOfStrings);
+    });
+
+    it('should contain `Luke Skywalker`', () => {
+      expect(starWars.all).to.include('Luke Skywalker');
+    });
+  });
+
+  describe('random', () => {
+    it('should return a random item from the starWars.all', () => {
+      const randomItem = starWars.random();
+      expect(starWars.all).to.include(randomItem);
+    });
   });
 });
